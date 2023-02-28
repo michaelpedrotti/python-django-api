3b7a5fcf953115469267416d9362da3d840ba9e6
Michael Pedrotti <michael.pedrotti.11@gmail.com>
2023-02-28 13:53:58 -0300

    Bugfix

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import ProfileModel
 from .serializers import ProfileSerializer
+import sys
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
+
     queryset = ProfileModel.objects.all()
     serializer_class = ProfileSerializer
+
+    def list(self, req):
+        res =  super().list(self, req)
+
+        # print(res.data, file=sys.stderr)
+
+        res.data = { "error": False, "rows": res.data }
+        return res
--- project/settings.py
+++ project/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     # 'django.contrib.sessions',
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
+    # https://www.django-rest-framework.org/
     'rest_framework',
     'app'
 ]
@@ -44,7 +45,9 @@ INSTALLED_APPS = [
 # }
 
 MIDDLEWARE = [
-    # 'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'app.middlewares.AuthenticationMiddleware',
+    'app.middlewares.AuthorizationMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
